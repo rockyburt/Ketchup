bf297182cc1f47744b04b978d939bbaa84d7704c
Rocky Burt <rocky.burt@gmail.com>
2021-09-19 11:42:13 -0230

    Simple isort cleanup

--- alembic/env.py
+++ alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import create_async_engine
 
 from alembic import context
-from ketchup import sqlamodels, base
+from ketchup import base, sqlamodels
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
